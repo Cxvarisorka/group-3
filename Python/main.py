c805189805c160f8dd9372f635896a9cca23148a
Cxvarisorka <130844113+Cxvarisorka@users.noreply.github.com>
2023-12-18 21:12:16 +0400

    version 0.0.1

---
+++ Python/main.py
@@ -0,0 +1,5 @@
+def calc_age(birth_year):
+    age = 2023 - birth_year
+    return age
+
+print(calc_age(2006))
---
+++ script.js
@@ -0,0 +1,10 @@
+let pElement = document.getElementById('name1');
+
+console.log(pElement);
+
+
+pElement = 'wefewfewfwefwef';
+
+console.log(pElement)
+
+
